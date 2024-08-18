68044e01696774aa28ecd188d49f0099199624cf
JeongYeonKim <kimjeongyeon113@gmail.com>
2024-08-18 14:30:43 +0900

    link button update

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,8 @@
-import { Box, Container, HStack, Skeleton, SkeletonCircle, SkeletonText, Stack, Text, VStack } from '@chakra-ui/react';
+import { Box, Button, Container, HStack,  Skeleton, SkeletonCircle, SkeletonText, Stack, Text, VStack } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
 import ISmallFarm from '../types';
 import { getAllSmallFarm } from '../api';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   const { data: allSmallFarm, isLoading: isAllSmallFarmLoading } = useQuery<ISmallFarm[]>({
@@ -10,5 +11,77 @@ export default function Home() {
   });
   const logoColor = ['#D6EFD8', '#80AF81', '#508D4E', '#1A5319'];
   console.log(!allSmallFarm);
-  return <VStack minH={'1200px'}></VStack>;
+  return <VStack minH={'1200px'}>
+    <Box>
+      <Text fontSize={"30px"}>soil sample</Text>
+      <HStack padding={"10px"}>
+
+      <Box
+      padding={5}
+      display={"flex"}
+      rounded={"10px"}
+      w={"70px"}
+      border={"2px solid"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      borderColor={logoColor[3]}
+      backgroundColor={logoColor[0]}
+      color={logoColor[3]}
+      _hover={{ bg: logoColor[3], color: logoColor[0] }}
+      >
+        <Link to={"/study/1"}>
+        <Text fontWeight={"600"} fontSize={"30px"}>1</Text></Link>
+      </Box>
+      <Box
+      padding={5}
+      display={"flex"}
+      rounded={"10px"}
+      w={"70px"}
+      border={"2px solid"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      borderColor={logoColor[3]}
+      backgroundColor={logoColor[0]}
+      color={logoColor[3]}
+      _hover={{ bg: logoColor[3], color: logoColor[0] }}
+      >
+        <Link to={"/study/2"}>
+        <Text fontWeight={"600"} fontSize={"30px"}>2</Text></Link>
+      </Box>
+      <Box
+      padding={5}
+      display={"flex"}
+      rounded={"10px"}
+      w={"70px"}
+      border={"2px solid"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      borderColor={logoColor[3]}
+      backgroundColor={logoColor[0]}
+      color={logoColor[3]}
+      _hover={{ bg: logoColor[3], color: logoColor[0] }}
+      >
+        <Link to={"/study/3"}>
+        <Text fontWeight={"600"} fontSize={"30px"}>3</Text></Link>
+      </Box>
+      <Box
+      padding={5}
+      display={"flex"}
+      rounded={"10px"}
+      w={"70px"}
+      border={"2px solid"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      borderColor={logoColor[3]}
+      backgroundColor={logoColor[0]}
+      color={logoColor[3]}
+      _hover={{ bg: logoColor[3], color: logoColor[0] }}
+      >
+        <Link to={"/study/4"}>
+        <Text fontWeight={"600"} fontSize={"30px"}>4</Text></Link>
+      </Box>
+      </HStack>
+    
+    </Box>
+  </VStack>;
 }
